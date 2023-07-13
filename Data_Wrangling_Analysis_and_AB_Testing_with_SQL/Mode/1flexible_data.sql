9e95cacca8689f580fb2fa5fbdd028b190a98504
Mohammad-shkir7 <139066095+Mohammad-shkir7@users.noreply.github.com>
2023-07-13 14:09:01 +0300

    Update 1flexible_data.sql

--- Data_Wrangling_Analysis_and_AB_Testing_with_SQL/Mode/1flexible_data.sql
+++ Data_Wrangling_Analysis_and_AB_Testing_with_SQL/Mode/1flexible_data.sql
@@ -1,1 +1,30 @@
-
+SELECT
+  event_id,
+  user_id,
+  event_time,
+  platform,
+  MAX(
+    CASE
+      -- usnig max to get rid of nulls 
+      WHEN parameter_name = 'item_id' THEN cast(parameter_value AS int)
+      ELSE NULL
+    END
+  ) AS item_id,
+  MAX(
+    CASE
+      -- max even works with strings in order to get rid of nulls 
+      WHEN parameter_name = 'referrer' THEN parameter_value
+      ELSE NULL
+    END
+  ) AS referrer -- MAX FUNCTION MUST BE USED FOR BOTH COLUMNS IN ORDER TO ELIMINATE NULLS
+FROM
+  dsv1069.events -- dsc1069 is the default database in this course
+WHERE
+  event_name = 'view_item'
+GROUP BY
+  event_id,
+  user_id,
+  event_time,
+  platform
+ORDER BY
+  event_id
